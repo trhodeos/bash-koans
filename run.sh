bec3b26455010842899954d66fc2199bb8ef8ebf
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2014-02-15 19:28:19 -0600

    Initial commit

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+# error handling hook.
+# we want to catch metadata about the test failure.
+error() {
+  local parent_lineno="$1"
+  local filename="$2"
+  local message="$3"
+
+  local line=$(head -n $parent_lineno $filename | tail -n 1)
+  if [[ -n "$message" ]] ; then
+    echo "Error on or near line ${parent_lineno} in ${filename}; ${message}:"
+  else
+    echo "Error on or near line ${parent_lineno} in ${filename}:"
+  fi
+  echo "  $line"
+  exit 1
+}
+
+trap 'error ${LINENO} ${BASH_SOURCE}' ERR
+
+source about_comments
+source about_variables
+source about_functions
+source about_control_statements
+source about_pipes
+source about_exit_codes
